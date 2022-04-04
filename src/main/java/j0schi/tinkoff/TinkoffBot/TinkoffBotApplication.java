36e728eaf6be1dad309edad61933e502c68299fa
j0schiTadamori <j0schitadamori@gmail.com>
2022-04-04 06:26:49 +0300

    Добавил Spring заготовку.

---
+++ src/main/java/j0schi/tinkoff/TinkoffBot/TinkoffBotApplication.java
@@ -0,0 +1,13 @@
+package j0schi.tinkoff.TinkoffBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinkoffBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/j0schi/tinkoff/TinkoffBot/TinkoffBotApplicationTests.java
@@ -0,0 +1,13 @@
+package j0schi.tinkoff.TinkoffBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
